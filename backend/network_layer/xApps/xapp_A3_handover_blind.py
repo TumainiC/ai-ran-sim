a99d04f5de05329bfdb2d2b6c8ae8f087ad516cf
ntutangyun <ntutangyun@gmail.com>
2025-05-17 16:56:12 +0100

    Update xapp_A3_handover_blind.py

--- backend/network_layer/xApps/xapp_A3_handover_blind.py
+++ backend/network_layer/xApps/xapp_A3_handover_blind.py
@@ -1,19 +1,21 @@
 from .xapp_base import xAppBase
 from utils import xAppControlAction
 
+
 class xAppA3HandoverBlind(xAppBase):
     """
     xApp that blindly performs handover actions upon receiving RRC measurement event A3.
     """
+
     def __init__(self, ric=None):
         super().__init__(ric=ric)
         self.enabled = True
-    
+
     def handle_rrc_meas_event_A3(self, event):
         ue = event["triggering_ue"]
         print(f"{self.xapp_id}: Received RRC measurement event A3 for UE {ue.ue_imsi}")
         print(event)
-        
+
         # blindly perform handover
         return xAppControlAction(
             action_type=xAppControlAction.ACTION_TYPE_HANDOVER,
