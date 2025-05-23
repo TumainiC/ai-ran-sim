af4e10e97503989baa6ed08fd518be9ad770c6cf
ntutangyun <ntutangyun@gmail.com>
2025-05-23 08:42:31 +0100

    Update network_knowledge_agent.py

--- backend/intelligence_layer/network_knowledge_agent.py
+++ backend/intelligence_layer/network_knowledge_agent.py
@@ -23,14 +23,14 @@ Use the tools to query the knowledge database of the simulated telecom network.
 To increase efficiency, you can use the bulk query tools to query multiple keys at once.
 You should always start with 
     "/docs/user_equipments" (documentation on UE-related knowledge base) 
-    "/docs/base_stations" (documentation on base station-related knowledge base)
-    "/docs/cells" (documentation on cell-related knowledge base)
+    "/docs/base_stations" (documentation on Base Station-related knowledge base)
+    "/docs/cells" (documentation on Cell-related knowledge base)
     "/docs/ric" (documentation on RIC-related knowledge base)
-    "/docs/sim_engine" (documentation on simulation-related knowledge base)
+    "/docs/sim_engine" (documentation on simulation execution-related knowledge base)
 
 The knowledge tools often returns a list of related knowledge keys.
 You should explore these related knowledge keys as well to gather more information to answer the user query wherever possible.
-Note that most elements in the URL patterns are plural (user_equipments, base_stations, cells, attributes, methods),
+Note that most elements in the query keys are plural (user_equipments, base_stations, cells, attributes, methods),
 except for the RIC and simulation engine, which are singular (ric, sim_engine).
 """,
     model=NON_REASONING_MODEL,
